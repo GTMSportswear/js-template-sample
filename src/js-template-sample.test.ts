a40bdbc88451966626c5b72070c3fd729cb05d9c
Daniel Longfellow <daniel.longfellow@igtm.com>
2017-01-06 09:48:58 -0600

    updated package.json

--- src/js-template-sample.test.ts
+++ src/js-template-sample.test.ts
@@ -1,5 +1,12 @@
-QUnit.module('Js Template Sample test');
+import { JsTemplateSample } from './js-template-sample';
+
+let parentContainer;
+
+QUnit.module('Js Template Sample test', {
+    beforeEach: () => parentContainer = document.createElement('div') 
+});
 
 QUnit.test('should create Js Template Sample', assert => {
-    
+    const templateSample = new JsTemplateSample(parentContainer);
+    assert.notEqual(parentContainer.innerHTML, null);
 });
--- src/js-template-sample.ts
+++ src/js-template-sample.ts
@@ -1,3 +1,8 @@
 export class JsTemplateSample {
-    
+    public output(){
+        const container = document.createElement('div');
+        this.parentContainer.appendChild(container);
+    }
+
+    constructor(private parentContainer: Element){ }
 }
